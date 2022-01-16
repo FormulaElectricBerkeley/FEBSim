cc3ed7967f4247404f66a6af89bb40281d53259a
Michael Huang <michael_huang@berkeley.edu>
2022-01-16 14:10:20 -0800

    Revert "Merge branch 'main' of https://github.com/weston-a-white/FEBSim into main"
    This reverts commit 84dd091a1668005dbb7ad6f2fbd583ef8cfd4ab2, reversing
    changes made to e61e6e64e2fc8015f4faea882895a9ba32d160bd.

--- EfficiencyCalc/EfficiencyCalc.m
+++ EfficiencyCalc/EfficiencyCalc.m
@@ -1,17 +1,12 @@
-function EffTime = EfficiencyCalc(OLResults, EffGraph,numMotors, lapsim)
+function EffTime = EfficiencyCalc(OLResults, EffGraph,numMotors)
     %Script to calculate overal car power consumption given OptimumLap 
     %results. Factors in motor innefficiency map that OL does not support.
-
 
 
     %Parameters
     %OLResults: Matrix from OptimumLap with columns, Time, RPM, Torque, and Brake Power
     %EffGraph: Matrix made from motor curves with columns, RPM, Torque, and Efficiency
     %numMotors: Number of Motors
-    %lapsim: Results from FEB trackLapModel
-
-    %FEB trackLapModel import (comment code out if using OptimumLap Results)
-    OLResults = cat(2, lapsim(:,1), lapsim(:,2)/(.254)*3.54*2*pi/60, lapsim(:,5)./4, zeros(length(lapsim),1));
         
     EffRPM = EffGraph(:,1);
     EffTorque = EffGraph(:,2);
