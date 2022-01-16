5b62e6b4491cdb43767ac1bf9c16778a5a7e0970
Michael Huang <michael_huang@berkeley.edu>
2022-01-16 14:12:46 -0800

    Revert "Revert "Implement lap plotting""
    This reverts commit e71452516c767737ad1f1d749936142f687147e3.

---
+++ lap plotting/trackDataSimple.m
@@ -0,0 +1,3 @@
+Type = ["Straight", "Left", "Straight","Right", "Straight", "Right", "Straight", "Right"];
+Length = [10, pi(), 10, 2*pi()/3, 10, pi()*6, 10, pi()*2];
+Radius = [0, 2, 0,2,0, 6, 0, 2];
---
+++ lap plotting/trackDataTest.m
@@ -0,0 +1,3 @@
+Type = ["Straight", "Left", "Straight","Left"];
+Length = [10, pi()*2, 10, pi()*2];
+Radius = [0, 2, 0,2];
