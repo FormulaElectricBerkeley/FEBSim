9ec9aa14fed22daa1c00d1bd67776456832bc79b
Andrew Song <andrew_song@berkeley.edu>
2023-03-25 10:52:59 -0700

    Implement traction limiting

--- TimeLogger.py
+++ TimeLogger.py
@@ -11,33 +11,43 @@ class TimeLogger:
         self.initial_acceleration = initial_acceleration
         self.final_distance = final_distance 
         self.tire_radius = .2667
-        self.driver_ratio = 3.53
-        self.acc_new = 9.75
+        self.driver_ratio = 5
+        self.acc_est = 7.85
         self.cf = .71
+        self.cf_long = 1.4
         self.A = 1.00
         self.crr = .02
         self.mass_with_driver = 300
+        self.L_wheelbase = 1.55
+        self.rear_bias = 0.55
+        self.H_cg = 0.31
         
     #functions
     def get_motor_speed(self, current_speed):
-        return current_speed/(2*pi*self.tire_radius)*2*pi*self.driver_ratio
+        return current_speed / (2 * pi * self.tire_radius) * 2 * pi * self.driver_ratio
 
     def get_power(self, current_motor_torque, current_motor_speed):
-        return current_motor_torque*current_motor_speed/1000
+        return current_motor_torque * current_motor_speed / 1000
 
-    def get_drag(self, cf, A, current_speed):
-        return cf*A*(current_speed**2)
+    def get_drag(self, current_speed):
+        return self.cf * self.A * (current_speed ** 2)
 
-    def get_rolling_resistance(self, mass_with_driver, crr):
-        return mass_with_driver*9.81*crr
+    def get_rolling_resistance(self):
+        return self.mass_with_driver * 9.81 * self.crr
 
-    def get_acceleration(self, current_get_motor_torque, driver_ratio, current_get_rolling_resistance, current_get_drag, tire_radius, mass_with_driver, acc_new):
-        a = (current_get_motor_torque*driver_ratio-current_get_rolling_resistance-current_get_drag)/tire_radius/mass_with_driver
-        return min(a, acc_new)
+    def get_acceleration(self, current_get_motor_torque, current_get_rolling_resistance, current_get_drag):
+        a = (current_get_motor_torque * self.driver_ratio - current_get_rolling_resistance - current_get_drag) / self.tire_radius / self.mass_with_driver
+        return min(a, self.get_acc_new())
 
-    def get_speed(self, initial_acceleration, time_delta, initial_speed):
-        return initial_acceleration*time_delta+initial_speed
-    
+    def get_speed(self, initial_acceleration, initial_speed):
+        return initial_acceleration * self.delta_time + initial_speed
+
+    def get_acc_new(self):
+        return self.cf_long * (self.rear_bias * self.mass_with_driver * 9.81 + self.get_load_transfer()) / self.mass_with_driver
+
+    def get_load_transfer(self):
+        return self.acc_est * self.H_cg * self.mass_with_driver / self.L_wheelbase
+
     my_list_motor_speed = [-1.00, .01, 5.31, 16.54, 27.77, 39.00, 50.23, 61.47, 72.70, 83.96, 95.16,
             106.40,117.63,128.86,139.39,150.62,161.85,173.09,184.32,195.55,206.78,218.02
             ,229.25,240.48,251.68,262.94,284.92,295.94,307.17,318.40,328.83,336.23,346.60
@@ -74,9 +84,9 @@ class TimeLogger:
      
         while distance < self.final_distance:
             new_distance = self.get_next_distance(distance, velocity, acceleration) 
-            new_velocity = self.get_speed(acceleration, self.delta_time, velocity)
+            new_velocity = self.get_speed(acceleration, velocity)
             current_time += self.delta_time
-            new_acceleration = self.get_acceleration(self.get_motor_torque(self.get_motor_speed(new_velocity)), self.driver_ratio , self.get_rolling_resistance(self.mass_with_driver, self.crr), self.get_drag(self. cf, self.A, new_velocity), self.tire_radius, self.mass_with_driver, self.acc_new)
+            new_acceleration = self.get_acceleration(self.get_motor_torque(self.get_motor_speed(new_velocity)), self.get_rolling_resistance(), self.get_drag(new_velocity))
             distance = new_distance
             velocity = new_velocity
             acceleration = new_acceleration
@@ -84,5 +94,6 @@ class TimeLogger:
         return(current_time)
             
 #example of the function below    
-newTimerLoger = TimeLogger(0, 0, .025, 9.45594300712411, 75)   
-newTimerLoger.get_time_given_distance()
+# newTimeLogger = TimeLogger(0, 0, .025, 9.45594300712411, 75)
+newTimeLogger = TimeLogger(0, 0, .025, 9.16722909636295, 75)
+newTimeLogger.get_time_given_distance()
